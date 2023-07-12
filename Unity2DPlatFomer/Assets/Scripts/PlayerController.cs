c22b4cd947032ec9e8c407e508116f7e62c11dc0
kimhyeok <kimmang940715@naver.com>
2023-07-12 09:11:54 +0900

    add PlayerMove

--- Unity2DPlatFomer/Assets/Scripts/PlayerController.cs
+++ Unity2DPlatFomer/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float maxSpeed = 0;
+    private Rigidbody2D rigid;
+
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        float h = Input.GetAxisRaw("Horizontal");
         
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed)
+        {
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        }
+        else if (rigid.velocity.x < maxSpeed * (-1))
+        {
+            rigid.velocity = new Vector2(maxSpeed* (-1), rigid.velocity.y);
+        }
     }
 }
