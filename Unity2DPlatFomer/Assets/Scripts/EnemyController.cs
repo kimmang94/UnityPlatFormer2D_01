efc49af00b8f67e6091d76ad328766259585757b
kimhyeok <kimmang940715@naver.com>
2023-07-14 06:32:01 +0900

    add EnemyMove invoke

--- Unity2DPlatFomer/Assets/Scripts/EnemyController.cs
+++ Unity2DPlatFomer/Assets/Scripts/EnemyController.cs
@@ -2,18 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyController : MonoBehaviour
 {
     private Rigidbody2D rigid = null;
 
+    [SerializeField] private int nextMove = 0;
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+
+        Invoke("NextState", 5f);
     }
 
     private void FixedUpdate()
     {
-        rigid.velocity = new Vector2(-1f, rigid.velocity.y);
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+    }
+
+    private void NextState()
+    {
+        nextMove = Random.Range(-1, 2);
+        
+        Invoke("NextState", 5f);
     }
 }
